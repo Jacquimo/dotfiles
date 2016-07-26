4b6c30d483e58ba013d32ad6510461e82e192fb8
Gray Houston <ghousto@purdue.edu>
2016-07-26 18:46:23 -0400

    Changed some code comments formatting.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -14,13 +14,15 @@
 #
 ############################
 
-########## Variables
+# ---------
+# Variables
+# ---------
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bash_profile"    					# list of files/folders to symlink in homedir
 
-##########
+# ---------
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
